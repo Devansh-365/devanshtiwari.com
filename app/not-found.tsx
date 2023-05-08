769bedd673772c4c17bbd5a3e56db9a696afce5f
Devansh-365 <devanshtiwari365@gmail.com>
2023-05-09 01:33:01 +0530

    feat(pages): add error

---
+++ app/not-found.tsx
@@ -0,0 +1,38 @@
+import React from "react"
+import Image from "next/image"
+
+import { AspectRatio } from "@/components/ui/aspect-ratio"
+import Link from "next/link"
+import { buttonVariants } from "@/components/ui/button"
+import { ArrowRight } from "lucide-react"
+
+const NotFound = () => {
+  return (
+    <section className="container flex min-h-[70vh] flex-col items-center justify-center text-center">
+      <AspectRatio
+        ratio={16 / 9}
+        className="bg-transparent dark:bg-transparent"
+      >
+        <Image
+          src="https://media.giphy.com/media/UHAYP0FxJOmFBuOiC2/giphy.gif"
+          alt="Photo by Alvaro Pinot"
+          fill
+          className="rounded-2xl object-cover"
+        />
+      </AspectRatio>
+      <h1 className="mt-6 text-2xl font-extrabold ">Site not found...</h1>
+      <p>The site you&apos;re searching for does not exist.</p>
+      <Link
+        className={buttonVariants({
+          variant: 'link',
+          className: 'w-fit underline mt-2',
+        })}
+        href='/'>
+        Back to home
+        <ArrowRight className='ml-2 h-4 w-4' />
+      </Link>
+    </section>
+  )
+}
+
+export default NotFound
