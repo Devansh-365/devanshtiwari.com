bfc7411aacab93c8bcf9958e8049eb796f5d7957
Devansh-365 <devanshtiwari365@gmail.com>
2023-08-20 19:10:39 +0530

    update header & add card

--- components/site-header.tsx
+++ components/site-header.tsx
@@ -26,36 +26,6 @@ export function SiteHeader() {
             <Link className="block md:hidden" href="/">
               <p className="text-2xl font-extrabold leading-snug">DT</p>
             </Link>
-            {/* <Link
-              href={siteConfig.links.github}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.gitHub className="h-5 w-5" />
-                <span className="sr-only">GitHub</span>
-              </div>
-            </Link>
-            <Link
-              href={siteConfig.links.twitter}
-              target="_blank"
-              rel="noreferrer"
-            >
-              <div
-                className={buttonVariants({
-                  size: "sm",
-                  variant: "ghost",
-                })}
-              >
-                <Icons.twitter className="h-5 w-5 fill-current" />
-                <span className="sr-only">Twitter</span>
-              </div>
-            </Link> */}
             <ThemeToggle />
           </nav>
         </div>
