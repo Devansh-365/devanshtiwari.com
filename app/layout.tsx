18e488db5c35d0c0c06bcb3d59073b8c0e912582
Devansh Tiwari <83155844+Devansh-365@users.noreply.github.com>
2026-03-23 23:42:39 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -49,20 +49,11 @@ export const metadata: Metadata = {
     title: siteConfig.title,
     description: siteConfig.description,
     siteName: siteConfig.name,
-    images: [
-      {
-        url: siteConfig.socialBanner,
-        width: 1200,
-        height: 630,
-        alt: siteConfig.name,
-      },
-    ],
   },
   twitter: {
     card: "summary_large_image",
     title: siteConfig.title,
     description: siteConfig.description,
-    images: [siteConfig.socialBanner],
     creator: siteConfig.twitterHandle,
   },
   icons: {
